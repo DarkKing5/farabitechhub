72f22e1b03f2e78e370c748ed95a34f5be7834cf
DarkKing5 <nazargozua@gmail.com>
2026-05-08 15:51:56 +0500

    Fix broken RU form URL

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ const i18n = {
 
 // ===== FORM URLS PER LANGUAGE =====
 const formUrls = {
-    ru: 'https://docs.google.com/forms/d/e/1FAIpQLSf0MDfAIQCAzPs-ODHomhlww-L9MJOvRBGSHeD6lqD3AYlQbcg/viewform',
+    ru: 'https://docs.google.com/forms/d/e/1FAIpQLSf0MDfAIQCAzPs-ODHomhlww-L9MJOvRBGSHeD6vTpDJBPzBA/viewform',
     kz: 'https://docs.google.com/forms/d/e/1FAIpQLSdbGH0GCV7N_iSgsLT5rWwsxOnvwdHc7UbBMzGvUMawmf1LQg/viewform',
     en: 'https://docs.google.com/forms/d/e/1FAIpQLSeduRP-QrxNCV5MSGnui92dxTJThJLKa7l4gFvwC4EmOnkKPw/viewform'
 };
